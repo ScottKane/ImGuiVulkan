54fd0f4b746150a9e5d288ed10e1a1fa4ee1e0f3
Scott Kane <scottpkane94@gmail.com>
2022-08-05 00:56:39 +0100

    Initial

---
+++ ImGuiVulkan/Program.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+
+namespace ImGuiVulkan;
+
+internal class Program
+{
+    private static void Main(string[] args) => new ImGuiVulkanApplication().Run();
+}
